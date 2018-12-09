4a058038b7cf3bcfe06bcdfa516fb9f4581ce007
vincenzo <vincenzo@VAIOVB>
2018-12-09 11:47:07 +0100

    Creata javadoc

--- develop/src/utility/vbruno/it/Calcola.java
+++ develop/src/utility/vbruno/it/Calcola.java
@@ -1,6 +1,15 @@
 package utility.vbruno.it;
-
+/**
+ * Classe di test per <strong>GitHub</strong>
+ * @author vincenzo
+ *
+ */
 public class Calcola {
+	/**
+	 * Questo metodo <u>raddoppia il parametro fornito in input</u>
+	 * @param x parametro, intero, da raddoppiare
+	 * @return il doppio del parametro
+	 */
 	public static int for2(int x)
 	{
 		return x*2;
