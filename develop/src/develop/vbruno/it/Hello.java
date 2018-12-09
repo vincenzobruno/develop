e1d23406f591700a4f6a8c008fe41b99ab1018e8
vincenzo <vincenzo@VAIOVB>
2018-12-09 11:41:05 +0100

    Nuovo package, nuova classe, modificata Hello.java

--- develop/src/develop/vbruno/it/Hello.java
+++ develop/src/develop/vbruno/it/Hello.java
@@ -1,10 +1,12 @@
 package develop.vbruno.it;
 
+import utility.vbruno.it.Calcola;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println(Calcola.for2(10));
 
 	}
 
---
+++ develop/src/utility/vbruno/it/Calcola.java
@@ -0,0 +1,8 @@
+package utility.vbruno.it;
+
+public class Calcola {
+	public static int for2(int x)
+	{
+		return x*2;
+	}
+}
